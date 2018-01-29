02a584798ce3b02a3a1d763bed5418b9eb0cee38
Simiyutin Boris <simiyutin.boris@yandex.ru>
2018-01-29 19:03:10 +0300

    concurrent clints

--- src/main/java/com/simiyutin/javaii/client/Main.java
+++ src/main/java/com/simiyutin/javaii/client/Main.java
@@ -7,9 +7,17 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+
+    // количесвто одновременно работающих лиентов
+    private static final int M = 10;
+
     public static void main(String[] args) throws IOException {
         String host = "localhost";
         int port = 11111;
@@ -30,8 +38,35 @@ public class Main {
         }
 
 
-        SerialClient client = new SerialClient();
-        client.start(host, port);
+        CyclicBarrier barrier = new CyclicBarrier(M);
+
+        List<Thread> clients = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            clients.add(new Thread(() -> {
+                try {
+                    barrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+                SerialClient client = new SerialClient();
+                try {
+                    client.start(host, port);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }));
+            clients.get(i).start();
+        }
+
+        for (Thread thread : clients) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         System.out.println("all right!");
+
     }
 }
