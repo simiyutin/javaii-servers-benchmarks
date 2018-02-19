94c1efee8357cb5c43077e9f401c9188fa9db21c
Simiyutin Boris <simiyutin.boris@yandex.ru>
2018-02-20 00:32:01 +0300

    factory update

--- src/main/java/com/simiyutin/javaii/testarch/ApplicationConfigurationFactory.java
+++ src/main/java/com/simiyutin/javaii/testarch/ApplicationConfigurationFactory.java
@@ -4,6 +4,8 @@ import com.simiyutin.javaii.client.Client;
 import com.simiyutin.javaii.client.TCPStatefulClient;
 import com.simiyutin.javaii.client.TCPStatelessClient;
 import com.simiyutin.javaii.server.Server;
+import com.simiyutin.javaii.server.tcp_async.TCPAsyncServer;
+import com.simiyutin.javaii.server.tcp_nonblocking.TCPNonBlockingServer;
 import com.simiyutin.javaii.server.tcp_serial.TCPSerialServer;
 import com.simiyutin.javaii.server.tcp_threadperclient.TCPThreadPerClientServer;
 import com.simiyutin.javaii.server.tcp_threadpool.TCPThreadPoolServer;
@@ -27,6 +29,14 @@ public class ApplicationConfigurationFactory {
             case "tcp_threadpool":
                 clientSupplier = () -> new TCPStatefulClient(conf);
                 serverSupplier = () -> new TCPThreadPoolServer(conf.port);
+                break;
+            case "tcp_nonblocking":
+                clientSupplier = () -> new TCPStatefulClient(conf);
+                serverSupplier = () -> new TCPNonBlockingServer(conf.port);
+                break;
+            case "tcp_async":
+                clientSupplier = () -> new TCPStatefulClient(conf);
+                serverSupplier = () -> new TCPAsyncServer(conf.port);
                 break;
         }
 
